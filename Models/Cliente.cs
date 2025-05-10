248870aff6807816bc56ad2e0f4118423b74baca
Edinaelson <sedinaelson@gmail.com>
2025-05-09 21:37:32 -0300

    add modal editar

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -7,12 +7,12 @@ public class Cliente
     public int Id { get; set; }
     
     [Required(ErrorMessage = "Nome obrigatorio")]
-    public string Nome { get; set; }
+    public string? Nome { get; set; }
     
     [Required(ErrorMessage = "Email obrigatorio")]
-    public string Email { get; set; }
+    public string? Email { get; set; }
     
-    public string Telefone { get; set; }
+    public string? Telefone { get; set; }
 
     [Required(ErrorMessage = "campo requerido")]
     public DateTime? DataNascimento { get; set; }
