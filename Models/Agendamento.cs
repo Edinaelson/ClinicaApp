c6711cdb878b4e54f7b390fb5164a1a838b79ce6
Edinaelson Galeno <sedinaelson@gmail.com>
2025-06-21 12:01:36 -0300

    correcao de salvamento de agenda

--- Models/Agendamento.cs
+++ Models/Agendamento.cs
@@ -1,14 +1,36 @@
-﻿namespace ClinicApp.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicApp.Models;
 
 public class Agendamento
 {
     public int Id { get; set; }
     public DateTime? DataAgendamento { get; set; }
-    // Hora de início (ex: 08:00 AM no dia da DataAgendamento)
     public TimeSpan? HoraInicio { get; set; }
-    // Hora de término/previsão (ex: 09:00 AM no dia da DataAgendamento)
     public TimeSpan? HoraFim { get; set; }
+
+    // Foreign Keys
+    public int TituloId { get; set; }
+    public int ClienteId { get; set; }
+    public int StatusId { get; set; }
+
+    // Navegação
     public Titulo Titulo { get; set; } = null!;
     public Cliente Cliente { get; set; } = null!;
     public Status Status { get; set; } = null!;
+
+    // Propriedades de uso no RadzenScheduler (não mapeadas no banco)
+    [NotMapped]
+    public DateTime Start => DataAgendamento.HasValue && HoraInicio.HasValue
+        ? DataAgendamento.Value.Date.Add(HoraInicio.Value)
+        : DateTime.MinValue;
+
+    [NotMapped]
+    public DateTime End => DataAgendamento.HasValue && HoraFim.HasValue
+        ? DataAgendamento.Value.Date.Add(HoraFim.Value)
+        : DateTime.MinValue;
+
+    //Texto que aparece em agenda
+    [NotMapped]
+    public string Text => $"{Cliente.Nome} - {Titulo.Texto}";
 }
